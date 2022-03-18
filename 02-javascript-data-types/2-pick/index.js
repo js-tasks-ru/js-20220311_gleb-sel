985337e8447f24b570a9b47a929803d78cc68c24
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-03-18 16:56:01 +0300

    solve sortPick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,10 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const pickedObj = Object.fromEntries(
+    Object.entries(obj).filter((el) => {
+      return fields.includes(el[0]);
+    })
+  );
+  return pickedObj;
 };
