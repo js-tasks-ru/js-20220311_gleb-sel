7d45a4255c6275a84b21b9f64dc2147842b7266d
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-04-01 18:24:40 +0300

    solve 05-01

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  constructor(message = "", {duration = 1000, type = "error"} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+  }
+
+  static oldMessage;
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          ${this.message}
+        </div>
+      </div>
+        `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  getMessage() {
+    return this.message ? `<div class="notification-body"> ${this.message} </div>` : "";
+  }
+
+  show(target) {
+    
+    if (NotificationMessage.oldMessage) {
+      NotificationMessage.oldMessage.remove();
+    }
+
+    setTimeout(() => this.remove(), this.duration);
+
+    target.after(this.element);
+    NotificationMessage.oldMessage = this.element;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    if (this.element) {
+      this.remove();
+    }
+    // this.element = null;
+  }
 }
+
+
+// npm run test -- 05-dom-document-loading/1-notification/index.spec.js
